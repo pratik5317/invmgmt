64431d43640a134caef6fec3708f2f685b9b1ebc
bhavik.ambani2013@gmail.com <bhavik.ambani2013@gmail.com@4f07a19c-b3bb-17e4-13e0-545acb9d8ae2>
2014-09-10 16:09:42 +0000

    ACL and Authentication - Bhavik Ambani

--- trunk/InvMgmt/src/java/com/tss/ocean/controller/AuthController.java
+++ trunk/InvMgmt/src/java/com/tss/ocean/controller/AuthController.java
@@ -4,14 +4,7 @@
  */
 package com.tss.ocean.controller;
 
-import com.tss.ocean.pojo.Users;
-import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,59 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class AuthController {
-
-    private static final Logger logger = Logger.getLogger(AuthController.class.getName());
-    
-    @RequestMapping(value="login.html",method= RequestMethod.POST)    
-    public String login(Model model, Users loginform, Locale locale, HttpServletRequest request) throws Exception {
- 
-        String username = loginform.getName();
-        String password = loginform.getPassword();
- 
-        // A simple authentication manager
-        if(username != null && password != null && !("").equals(username) 
-                && !("").equals(password)){                           
-                
-//            if( username.equals(settings.getString("username")) &&  password.equals(settings.getObject("password")) ){
-                //return "welcome";
-                request.getSession().setAttribute("LOGGEDIN_USER", loginform);
-                //to do ..
-                //set the title here...
-                request.getSession().setAttribute("title", "Management System 1.0");
-                
-                return "redirect:/";
-//                return "redirect:/?lang=ar";//to set locale here...
-//            }else{
-//                return "redirect:/login.failed";
-//            }
-        }else{
-            return "redirect:/login.html";
-        }  
-    }
     
     @RequestMapping(value="/login.html",method= RequestMethod.GET)
-    public String getlogin(Model model, Users loginform) throws Exception {
-        logger.log(Level.OFF,"getlogin called.");        
+    public String getlogin() throws Exception {        
         return "login";
-    }
-    
-    @RequestMapping(value="/logout.html",method= RequestMethod.GET)
-    public String logout(Model model, Users loginform,HttpServletRequest request) throws Exception {
-        logger.log(Level.OFF,"logout called.");
-        request.getSession().invalidate();
-        model.addAttribute("loginAttribute", loginform);
-        return "login";
-    }
-
-    @RequestMapping(value="/",method= RequestMethod.GET)
-    public String index(HttpServletRequest request) throws Exception {
-        logger.log(Level.OFF,"index called.");
-        boolean allow = request.isUserInRole("ROLE_USER");
-        if(allow) {
-            return "index";
-        }
-        else {
-            return "login";
-        }
-    }
+    }   
 }
